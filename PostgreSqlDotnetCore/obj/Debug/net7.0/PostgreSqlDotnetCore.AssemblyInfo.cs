9d6cdf448cf6360fbd241c59f3d2bc93b609271f
harshasaijagu <harshasaijagu@usf.edu>
2024-11-21 18:26:56 +0530

    Upload files to github

--- PostgreSqlDotnetCore/obj/Debug/net7.0/PostgreSqlDotnetCore.AssemblyInfo.cs
+++ PostgreSqlDotnetCore/obj/Debug/net7.0/PostgreSqlDotnetCore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostgreSqlDotnetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125fdad8d6dadd8dc139b17bac419adf38518fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostgreSqlDotnetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostgreSqlDotnetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
